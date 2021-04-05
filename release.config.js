8a4d23e9824f33055866764314840cddecbf7251
Leonardo Silva <leosilvadev@gmail.com>
2021-04-05 15:41:27 +0200

    feat: add artifacts into the release

--- release.config.js
+++ release.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-	branches: "main",
-	repositoryUrl: "https://github.com/leosilvadev/github-actions-example",
-	plugins: [
-		"@semantic-release/commit-analyzer",
-		"@semantic-release/release-notes-generator",
-		"@semantic-release/github",
-	],
+    branches: "main",
+    repositoryUrl: "https://github.com/leosilvadev/github-actions-example",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator", ["@semantic-release/github", {
+            assets: [
+                { path: "build.zip", label: "Build" },
+                { path: "coverage.zip", label: "Coverage" }
+            ]
+        }],
+    ],
 };

2acbb997e305641404d1813bf4c617b8d6cfb323
Leonardo Silva <leosilvadev@gmail.com>
2021-04-05 17:38:58 +0200

    fix: code format

--- release.config.js
+++ release.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/leosilvadev/github-actions-example",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator", ["@semantic-release/github", {
-            assets: [
-                { path: "build.zip", label: "Build" },
-                { path: "coverage.zip", label: "Coverage" }
-            ]
-        }],
-    ],
+	branches: "main",
+	repositoryUrl: "https://github.com/leosilvadev/github-actions-example",
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{ path: "build.zip", label: "Build" },
+					{ path: "coverage.zip", label: "Coverage" },
+				],
+			},
+		],
+	],
 };
